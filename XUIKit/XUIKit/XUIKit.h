5a625752fe014b6f0f84aaa44efd7fbe33a6fd3b
HoCheung <Jovi@jovis-macbook-air.local>
2017-06-09 18:40:50 +0800

    Initialized XUIKit porject ✌️

---
+++ XUIKit/XUIKit/XUIKit.h
@@ -0,0 +1,19 @@
+//
+//  XUIKit.h
+//  XUIKit
+//
+//  Created by Jovi on 6/9/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XUIKit.
+FOUNDATION_EXPORT double XUIKitVersionNumber;
+
+//! Project version string for XUIKit.
+FOUNDATION_EXPORT const unsigned char XUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XUIKit/PublicHeader.h>
+
+
