7a1f19a188ef89bddc9598100a42c1dfb784e652
HsiangHo <ho_cheung@qq.com>
2017-06-18 10:23:15 +0800

    get rid of Nullability Annotations warning

--- XUIKit/XUIKit/NSControl+XUIAdditions.h
+++ XUIKit/XUIKit/NSControl+XUIAdditions.h
@@ -18,19 +18,19 @@
 // add target/action for particular event. you can call this multiple times and you can specify multiple target/actions for a particular event.
 // passing in nil as the target goes up the responder chain. The action may optionally include the sender and the event in that order
 // the action cannot be NULL. Note that the target is not retained.
-- (void)addTarget:(nullable id)target action:(SEL)action forControlEvents:(XUIControlEvents)controlEvents;
-- (void)addActionForControlEvents:(XUIControlEvents)controlEvents block:(void(^)(void))action;
+- (void)addTarget:(nullable id)target action:(nonnull SEL)action forControlEvents:(XUIControlEvents)controlEvents;
+- (void)addActionForControlEvents:(XUIControlEvents)controlEvents block:(nonnull void(^)(void))action;
 
 // remove the target/action for a set of events. pass in NULL for the action to remove all actions for that target
 - (void)removeTarget:(nullable id)target action:(nullable SEL)action forControlEvents:(XUIControlEvents)controlEvents;
 - (void)removeAllTargets;
 
 
-- (NSSet *)allTargets;
+- (nonnull NSSet *)allTargets;
 - (XUIControlEvents)allControlEvents;
-- (NSArray *)actionsForTarget:(id)target forControlEvent:(XUIControlEvents)controlEvent;
+- (nullable NSArray *)actionsForTarget:(nullable id)target forControlEvent:(XUIControlEvents)controlEvent;
 
-- (void)sendAction:(SEL)action to:(id)target forEvent:(NSEvent *)event;
+- (void)sendAction:(nonnull SEL)action to:(nullable id)target forEvent:(nullable NSEvent *)event;
 - (void)sendActionsForControlEvents:(XUIControlEvents)controlEvents;
 
 @end
