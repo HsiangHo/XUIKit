894cfc521daa7c6e9d00670c563a9aa3606cc039
HsiangHo <ho_cheung@qq.com>
2017-06-19 19:37:03 +0800

    add XUIImageView

---
+++ XUIKit/XUIKit/XUIImageView.h
@@ -0,0 +1,13 @@
+//
+//  XUIImageView.h
+//  XUIKit
+//
+//  Created by Jovi on 6/19/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface XUIImageView : NSImageView
+
+@end
---
+++ XUIKit/XUIKit/XUIImageView.m
@@ -0,0 +1,19 @@
+//
+//  XUIImageView.m
+//  XUIKit
+//
+//  Created by Jovi on 6/19/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import "XUIImageView.h"
+
+@implementation XUIImageView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
