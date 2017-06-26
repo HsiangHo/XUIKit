480f3c67441fe4363a7a386401dfcfcdb253c5f1
HsiangHo <ho_cheung@qq.com>
2017-06-26 19:15:38 +0800

    add performKeyEquivalent to XUITextField

--- XUIKit/XUIKit/XUITextField.h
+++ XUIKit/XUIKit/XUITextField.h
@@ -7,6 +7,7 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "NSTextField+XUIAdditions.h"
 
 @interface XUITextField : NSTextField
 
--- XUIKit/XUIKit/XUITextField.m
+++ XUIKit/XUIKit/XUITextField.m
@@ -16,4 +16,21 @@
     // Drawing code here.
 }
 
+//Respond to shortcut key
+- (BOOL)performKeyEquivalent:(NSEvent *)event {
+    if (NSCommandKeyMask == ([event modifierFlags] & NSDeviceIndependentModifierFlagsMask)) {
+        // The command key is the ONLY modifier key being pressed.
+        if ([[event charactersIgnoringModifiers] isEqualToString:@"x"]) {
+            return [NSApp sendAction:@selector(cut:) to:[[self window] firstResponder] from:self];
+        } else if ([[event charactersIgnoringModifiers] isEqualToString:@"c"]) {
+            return [NSApp sendAction:@selector(copy:) to:[[self window] firstResponder] from:self];
+        } else if ([[event charactersIgnoringModifiers] isEqualToString:@"v"]) {
+            return [NSApp sendAction:@selector(paste:) to:[[self window] firstResponder] from:self];
+        } else if ([[event charactersIgnoringModifiers] isEqualToString:@"a"]) {
+            return [NSApp sendAction:@selector(selectAll:) to:[[self window] firstResponder] from:self];
+        }
+    }
+    return [super performKeyEquivalent:event];
+}
+
 @end
