934d8062d37de3a9e1b8cb82d231d236e1410699
HsiangHo <ho_cheung@qq.com>
2017-07-12 20:58:47 +0800

    added XUIActivityIndicatorBallRotateChaseView

---
+++ XUIKit/XUIKit/XUIActivityIndicatorBallRotateChaseView.h
@@ -0,0 +1,14 @@
+//
+//  XUIActivityIndicatorBallRotateChaseView.h
+//  XUIKit
+//
+//  Created by Jovi on 7/12/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "XUIActivityIndicatorViewProtocol.h"
+
+@interface XUIActivityIndicatorBallRotateChaseView : NSObject<XUIActivityIndicatorViewProtocol>
+
+@end
---
+++ XUIKit/XUIKit/XUIActivityIndicatorBallRotateChaseView.m
@@ -0,0 +1,13 @@
+//
+//  XUIActivityIndicatorBallRotateChaseView.m
+//  XUIKit
+//
+//  Created by Jovi on 7/12/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import "XUIActivityIndicatorBallRotateChaseView.h"
+
+@implementation XUIActivityIndicatorBallRotateChaseView
+
+@end
