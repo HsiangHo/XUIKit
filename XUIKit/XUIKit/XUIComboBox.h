3654dab738071f4a6e9f1b4a33ab26b75b8fd859
HsiangHo <ho_cheung@qq.com>
2017-07-17 09:16:17 +0800

    added XUIComboBox

---
+++ XUIKit/XUIKit/XUIComboBox.h
@@ -0,0 +1,13 @@
+//
+//  XUIComboBox.h
+//  XUIKit
+//
+//  Created by Jovi on 7/17/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface XUIComboBox : NSComboBox
+
+@end
---
+++ XUIKit/XUIKit/XUIComboBox.m
@@ -0,0 +1,19 @@
+//
+//  XUIComboBox.m
+//  XUIKit
+//
+//  Created by Jovi on 7/17/17.
+//  Copyright © 2017 Jovi. All rights reserved.
+//
+
+#import "XUIComboBox.h"
+
+@implementation XUIComboBox
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
