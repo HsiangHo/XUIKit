84999f75b4be368fadec5f9d861ff6e446745768
HsiangHo <ho_cheung@qq.com>
2017-07-19 19:51:21 +0800

    update Methods

--- XUIKit/XUIKit/XUITextFieldCell.m
+++ XUIKit/XUIKit/XUITextFieldCell.m
@@ -9,14 +9,6 @@
 #import "XUITextFieldCell.h"
 #import "XUITextField.h"
 
-NSRect Rect4Text(NSRect rect,NSEdgeInsets textEdgeInsets){
-    rect.origin.x = textEdgeInsets.left;
-    rect.origin.y = textEdgeInsets.bottom;
-    rect.size.width = rect.size.width - textEdgeInsets.left - textEdgeInsets.right;
-    rect.size.height = rect.size.height - textEdgeInsets.top - textEdgeInsets.bottom;
-    return rect;
-}
-
 @implementation XUITextFieldCell{
     struct{
         unsigned int editing:1;
@@ -31,7 +23,7 @@ NSRect Rect4Text(NSRect rect,NSEdgeInsets textEdgeInsets){
     _textfieldFlags.editing = YES;
     if ([controlView isKindOfClass:[XUITextField class]]) {
         XUITextField *tf = (XUITextField *)controlView;
-        rect = Rect4Text(rect,[tf textEdgeInsets]);
+        rect = [self __rect4Text:rect withEdageInsets:[tf textEdgeInsets]];
     }
     return [super editWithFrame:rect inView:controlView editor:textObj delegate:delegate event:event];
 }
@@ -41,7 +33,7 @@ NSRect Rect4Text(NSRect rect,NSEdgeInsets textEdgeInsets){
     _textfieldFlags.editing = YES;
     if ([controlView isKindOfClass:[XUITextField class]]) {
         XUITextField *tf = (XUITextField *)controlView;
-        aRect = Rect4Text(aRect,[tf textEdgeInsets]);
+        aRect = [self __rect4Text:aRect withEdageInsets:[tf textEdgeInsets]];
     }
     [super selectWithFrame:aRect inView:controlView editor:textObj delegate:anObject start:selStart length:selLength];
 }
@@ -54,9 +46,17 @@ NSRect Rect4Text(NSRect rect,NSEdgeInsets textEdgeInsets){
 - (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView{
     if ([controlView isKindOfClass:[XUITextField class]]) {
         XUITextField *tf = (XUITextField *)controlView;
-        cellFrame = Rect4Text(cellFrame,[tf textEdgeInsets]);
+        cellFrame = [self __rect4Text:cellFrame withEdageInsets:[tf textEdgeInsets]];
     }
     [super drawInteriorWithFrame:cellFrame inView:controlView];
+}
+
+-(NSRect)__rect4Text:(NSRect) rect withEdageInsets:(NSEdgeInsets)textEdgeInsets{
+    rect.origin.x = textEdgeInsets.left;
+    rect.origin.y = textEdgeInsets.bottom;
+    rect.size.width = rect.size.width - textEdgeInsets.left - textEdgeInsets.right;
+    rect.size.height = rect.size.height - textEdgeInsets.top - textEdgeInsets.bottom;
+    return rect;
 }
 
 @end
