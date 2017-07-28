22208d6ee552224a67fb9e3e984480f87c5c4891
HsiangHo <ho_cheung@qq.com>
2017-07-28 22:47:29 +0800

    added interface for XUIWindow

--- XUIKit/XUIKit/XUIWindow.h
+++ XUIKit/XUIKit/XUIWindow.h
@@ -8,6 +8,16 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class XUIWindowSystemButtonView;
+@class XUIView;
+@class XUILabel;
+
+NS_ASSUME_NONNULL_BEGIN
 @interface XUIWindow : NSWindow
 
+@property (nonatomic,strong)    XUIWindowSystemButtonView   *systemButtonView;
+@property (nonatomic,strong)    XUIView                     *headerView;
+@property (nonatomic,strong)    XUILabel                    *windowTitle;
+
 @end
+NS_ASSUME_NONNULL_END
