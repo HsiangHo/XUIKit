9a5f9b35b5b0da5eb2410ab2fe538af93f5854a0
HsiangHo <ho_cheung@qq.com>
2017-07-29 15:44:57 +0800

    update XUIWindow

--- XUIKit/XUIKit/XUIWindow.h
+++ XUIKit/XUIKit/XUIWindow.h
@@ -15,9 +15,16 @@
 NS_ASSUME_NONNULL_BEGIN
 @interface XUIWindow : NSWindow
 
-@property (nonatomic,strong)    XUIWindowSystemButtonView   *systemButtonView;
-@property (nonatomic,strong)    XUIView                     *headerView;
-@property (nonatomic,strong)    XUILabel                    *windowTitle;
+@property (nonatomic,strong,readonly)       XUIWindowSystemButtonView   *systemButtonView;
+@property (nonatomic,strong,readonly)       XUIView                     *headerView;
+
+@property (nonatomic,assign)                NSSize                      minimumSize;
+@property (nonatomic,assign)                NSSize                       maximumSize;
+
+//Title is a subview of the header view.
+@property (nonatomic,assign)                NSRect                      titleFrame;
+@property (nonatomic,copy)                  NSColor                     *titleColor;
+@property (nonatomic,copy)                  NSFont                      *titleFont;
 
 @end
 NS_ASSUME_NONNULL_END
--- XUIKit/XUIKit/XUIWindow.m
+++ XUIKit/XUIKit/XUIWindow.m
@@ -7,7 +7,145 @@
 //
 
 #import "XUIWindow.h"
+#import "XUIView.h"
+#import "XUIWindowSystemButtonView.h"
+#import "XUILabel.h"
+#import "NSColor+XUIAdditions.h"
 
-@implementation XUIWindow
+@implementation XUIWindow{
+    XUIWindowSystemButtonView           *_systemButtonView;
+    XUIView                             *_headerView;
+    XUILabel                            *_windowTitle;
+    NSRect                              _titleFrame;
+    NSSize                              _minimumSize;
+    NSSize                              _maximumSize;
+
+    __weak NSView                       *_titlebarContainerView;
+}
+
+#pragma mark - Override Methods
+
+-(instancetype)init{
+    if (self = [super init]) {
+        [self __initializeXUIWindow];
+    }
+    return self;
+}
+
+-(instancetype)initWithContentRect:(NSRect)contentRect styleMask:(NSWindowStyleMask)style backing:(NSBackingStoreType)bufferingType defer:(BOOL)flag{
+    if (self = [super initWithContentRect:contentRect styleMask:style backing:bufferingType defer:flag]) {
+        [self __initializeXUIWindow];
+    }
+    return self;
+}
+
+-(instancetype)initWithContentRect:(NSRect)contentRect styleMask:(NSWindowStyleMask)style backing:(NSBackingStoreType)bufferingType defer:(BOOL)flag screen:(NSScreen *)screen{
+    if (self = [super initWithContentRect:contentRect styleMask:style backing:bufferingType defer:flag screen:screen]) {
+        [self __initializeXUIWindow];
+    }
+    return self;
+}
+
+-(BOOL)canBecomeKeyWindow{
+    return YES;
+}
+
+-(BOOL)canBecomeMainWindow {
+    return YES;
+}
+
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowDidResizeNotification object:nil];
+}
+
+-(void)windowDidResize:(NSNotification *)notification{
+    [self __adjustContentViewLayout];
+}
+
+-(void)setTitle:(NSString *)title{
+    [super setTitle:title];
+    [_windowTitle setText:title];
+    [self __adjustContentViewLayout];
+}
+
+-(NSSize)windowWillResize:(NSWindow *)sender toSize:(NSSize)frameSize{
+    NSSize size = frameSize;
+    if (frameSize.height < _minimumSize.height) {
+        size.height = _minimumSize.height;
+    }else if(frameSize.height > _maximumSize.height) {
+        size.height = _maximumSize.height;
+    }
+    
+    if (frameSize.width < _minimumSize.width){
+        size.width = _minimumSize.width;
+    }else if(frameSize.width > _maximumSize.width) {
+        size.width = _maximumSize.width;
+    }
+    return size;
+}
+
+#pragma mark - Private Methods
+
+-(void)__initializeXUIWindow{
+    NSArray *arraySubviews = self.contentView.superview.subviews;
+    for(NSView *view in arraySubviews){
+        if([[view className] isEqualToString:@"NSTitlebarContainerView"]){
+            [view setHidden:YES];
+            _titlebarContainerView = view;
+            break;
+        }
+    }
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidResize:) name:NSWindowDidResizeNotification object:nil];
+    _headerView = [[XUIView alloc] initWithFrame:NSMakeRect(0, 0, 0, 40)];
+    _systemButtonView = [[XUIWindowSystemButtonView alloc] initWithFrame:NSMakeRect(10, 10, 63, 18)];
+    _titleFrame = NSMakeRect(0, 10, 0, 0);
+    _windowTitle = [[XUILabel alloc] initWithFrame:_titleFrame];
+    [_windowTitle setText:@"Window"];
+    [_windowTitle setFont:[NSFont fontWithName:@"Helvetica Neue" size:15]];
+    [_windowTitle setAlignment:NSCenterTextAlignment];
+    [_windowTitle setTextColor:[NSColor colorWithHex:@"#34324a" alpha:1.0]];
+    [_headerView addSubview:_systemButtonView];
+    [_headerView addSubview:_windowTitle];
+    [self.contentView addSubview:_headerView];
+    _minimumSize = NSMakeSize(300, 200);
+    _maximumSize = NSMakeSize(10000, 10000);
+    [self __adjustContentViewLayout];
+}
+
+-(void)__adjustContentViewLayout{
+    [self.contentView setFrame: self.contentView.superview.bounds];
+    NSRect rctContent = self.contentView.bounds;
+    NSRect rctHeaderView = _headerView.frame;
+    
+    rctHeaderView.size.width = NSWidth(rctContent);
+    rctHeaderView.origin.x = 0;
+    rctHeaderView.origin.y = NSHeight(rctContent) - NSHeight(rctHeaderView);
+    [_headerView setFrame:rctHeaderView];
+    
+    _titleFrame.size.height = _windowTitle.heightOfText;
+    _titleFrame.size.width = _windowTitle.widthOfText * 1.2;
+    _titleFrame.origin.x = (int)((NSWidth(rctContent) - NSWidth(_titleFrame)) / 2);
+    [_windowTitle setFrame:_titleFrame];
+}
+
+#pragma mark - Public Methods
+
+-(void)setTitleFont:(NSFont *)titleFont{
+    _windowTitle.font = titleFont;
+    [self __adjustContentViewLayout];
+}
+
+-(NSFont *)titleFont{
+    return _windowTitle.font;
+}
+
+-(void)setTitleColor:(NSColor *)titleColor{
+    _windowTitle.textColor = titleColor;
+    [self __adjustContentViewLayout];
+}
+
+-(NSColor *)titleColor{
+    return _windowTitle.textColor;
+}
 
 @end
